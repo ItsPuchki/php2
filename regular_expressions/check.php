e9fc2717f6ff9716b28e5639a7b1b0632af1a90d
Jason <kevinblank72@gmail.com>
2019-05-01 12:25:38 +0200

    index is gereed, check is nog niet af. styling is er nog niet.

---
+++ regular_expressions/check.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $num = $_POST['num'];
+  $postal = $_POST['postal'];
+  $email = $_POST['email'];
+  $address = $_POST['address'];
+  $homeNum = $_POST['homeNum'];
+}
+ ?>
--- regular_expressions/index.php
+++ regular_expressions/index.php
@@ -5,12 +5,17 @@
     <title></title>
   </head>
   <body>
-    <form type="get" action="">
-      <input type="text" name="form" placeholder="vul iets in">
-      <input type="submit" name="submit" placeholder="send"></input>
+    <form class="contact-form" action="contactform.php" method="post"
+      <input type="text" name="name" placeholder="Full name">
+      <input type="text" name="num" placeholder="Phonenumber">
+      <input type="text" name="postal" placeholder="Postalcode">
+      <input type="text" name="email" placeholder="E-mail">
+      <input type="text" name="address" placeholder="Address">
+      <input type="text" name="homeNum" placeholder="Home number">
+      <button type="submit" name="submit">Sign up</input>
     </form>
     <?php
-    include('header.php');
+    include('check.php');
      ?>
   </body>
 </html>
