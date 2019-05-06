e4a389e4bdce6fc3d57c2867670d99766ca1ab5f
Jason <kevinblank72@gmail.com>
2019-05-06 11:30:26 +0200

    check.php bijgewerkt en heeft nu meer preg_match_all echos.

--- regular_expressions/check.php
+++ regular_expressions/check.php
@@ -1,4 +1,5 @@
 <?php
+$name = "kevin__";
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $num = $_POST['num'];
@@ -7,4 +8,14 @@ if (isset($_POST['submit'])) {
   $address = $_POST['address'];
   $homeNum = $_POST['homeNum'];
 }
- ?>
+echo "name is $name <br>";
+echo "number is $num<br>";
+echo "postalnumber is $postal<br>";
+echo "E-mailadress is $email<br>";
+echo (preg_match_all("/[a-zA-Z]/",$name));
+echo (preg_match_all("/0-9/",$num));
+echo (preg_match_all("/[a-zA-Z0-9]/",$postal));
+echo (preg_match_all("/[a-zA-Z0-9]/",$email));
+{
+}
+?>
--- regular_expressions/index.php
+++ regular_expressions/index.php
@@ -5,14 +5,8 @@
     <title></title>
   </head>
   <body>
-    <form class="contact-form" action="contactform.php" method="post"
-      <input type="text" name="name" placeholder="Full name">
-      <input type="text" name="num" placeholder="Phonenumber">
-      <input type="text" name="postal" placeholder="Postalcode">
-      <input type="text" name="email" placeholder="E-mail">
-      <input type="text" name="address" placeholder="Address">
-      <input type="text" name="homeNum" placeholder="Home number">
-      <button type="submit" name="submit">Sign up</input>
+    <form class="contact-form" action="contactform.php" method="post">
+      <button type="submit" name="submit">Sign up</button>
     </form>
     <?php
     include('check.php');
